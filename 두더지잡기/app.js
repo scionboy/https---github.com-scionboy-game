d0e7c59de9a3b2afa7097537c3095fad98ffaa05
dojuun2 <djp9816@naver.com>
2022-10-24 16:16:35 +0900

    3라운드까지 완료

--- 두더지잡기/app.js
+++ 두더지잡기/app.js
@@ -1,30 +1,28 @@
 let preNum = 1;     // 이전 랜덤숫자
-let score = 0;  // 누적 점수
 let hideSec = 2000; // 두더지 나왔다가 들어갈때까지의 시간
 let showSec = 3000; // 두더지 들어갔다가 나올때까지의 시간
 let round = 1;  // 라운드 누적 변수
+let num = 17;    // img의 class를 증가시킬 변수
+var rowlevel = 4;
 
-let wingrade=10; // 다음라운드 점수
+let wingrade = 10; // 다음라운드 점수
 
-let round_test=4;
+let round_test = 4;
 
 // gradeSum : 누적 점수
 // ttime = 두더지 올라온 시점의 현재 시간
-
 var gradeSum = 0;
 var ttime = 0;
 
 // 타이머 설정
 var timerId; // 타이머 끄는 변수
-var time = 4; // 타이머 몇초로 할지
+var time = 0; // 타이머 몇초로 할지
 
 const $map = document.querySelector('.map');
 $map.addEventListener('click', function (e) {
     if (!e.target.matches('.map img')) return;
-    console.log('이미지 클릭:', e.target);
     touch();
     hideImg();
-    console.log("점수는 ", gradeSum);
     printScore();
 });
 
@@ -45,15 +43,13 @@ function getTime() {
 
 // 두더지 출력 이벤트
 function printImg(hideSecP) {
-    let ranNum = Math.floor(Math.random() * (round_test*round_test));  // 랜덤 숫자 (0~9)
+    let ranNum = Math.floor(Math.random() * (round_test * round_test));  // 랜덤 숫자 (0~9)
     if (ranNum === 0) { // 랜덤숫자가 0이면
         ranNum++;   // 0을 1로
     }
     const $item = document.querySelector('.item' + ranNum);
     ttime = getTime();
 
-    console.log(getTime());
-    console.log($item);
     $item.style.display = 'block';
     preNum = ranNum;
 
@@ -68,68 +64,116 @@ function hideImg() {
     $item.style.display = 'none';
 }
 
-// 게임 시작 이벤트
+// 게임 시작 함수
 function startGame(hideSecP, showSecP) {
     if (confirm('게임 시작?')) {
         gradeSum = 0;
         time = 10;
         inter_val();
         let interval = setInterval(() => {
+            // 게임 종료 이벤트
+            document.getElementById('stop').addEventListener('click', function () {
+                if (confirm('게임을 종료하시겠습니까?')) {
+                    clearInterval(interval);    // 반복 중단
+                    timer_stop();
+                    document.querySelector(".timer").textContent = time;
+                    // gradeSum = 0;
+                    // hideImg();
+                    // printScore();
+                    // timer_stop();
+                }
+            })
             printImg(hideSecP);
-            if (time == 0 || gradeSum>=wingrade) {
+            if (time == 0 || gradeSum >= wingrade) {
                 winCheck();
                 clearInterval(interval);    // 반복 중단
             }
         }, showSecP);
     }
 }
+// // 게임 종료 함수
+// function stopGame() {
+//     if (confirm('게임을 종료하시겠습니까?')) {
+//         gradeSum = 0;
+//         hideImg();
+//         printScore();
+//         timer_stop();
+//     }
+// }
+
+// 게임 시작 이벤트
 document.getElementById('start').addEventListener('click', function () {
     startGame(hideSec, showSec);    // 2000, 3000
 })
 
-let num = 17;    // img의 class를 증가시킬 변수
-// 새 행, 열 추가
-var rowlevel=4;
+
+
+// 다음 라운드 함수
 function nextRound() {
     round++;
-    
-    if (round==2) {
-        hideSec=1000;
-        showSec=1500;
-    } else if(round==3){
-        hideSec=500;
-        showSec=1000;
-    }
-    startGame(hideSec, showSec);
-    const $map = document.querySelector('.map');
-    
-    // 행 추가
-    const newRow = $map.insertRow();
+    const $map = document.querySelector('.map');// 테이블
+
+    const newRow = $map.insertRow();    // 행 추가
     for (let i = 0; i < rowlevel; i++) {
-        var firstRow = document.querySelector('.map').rows[i];
-        var x = firstRow.insertCell(i);
-        x.innerHTML='<img src="img/두더지.jpg" alt="두더지" class="item' + num + '"><div class="hole"></div>';
+        // const firstRow = $map.rows[i];
+        // const x = firstRow.insertCell(i);
+        // x.innerHTML='<img src="img/두더지.jpg" alt="두더지" class="item' + num + '"><div class="hole"></div>';
         const newCell = newRow.insertCell(i);
         newCell.innerHTML = '<img src="img/두더지.jpg" alt="두더지" class="item' + num + '"><div class="hole"></div>';
         num++;
     }
 
+    const $holes = document.querySelectorAll('.hole');  // 구멍
+    const $td = document.querySelectorAll('td');   // td
+    if (round == 2) {   // 2라운드 일때
+        hideSec = 800;
+        showSec = 1300;
+        for (let $hole of $holes) {
+            $hole.style.height = '45px';
+        }
+        for (let $cells of $td) {
+            console.log($cells);
+            $cells.style.height = "130px";
+        }
+    } else if (round == 3) {    // 3라운드 일때
+        hideSec = 500;
+        showSec = 1000;
+        for (let $hole of $holes) {
+            $hole.style.height = '40px';
+            $hole.style.marginTop = '70px';
+        }
+        for (let $cells of $td) {
+            console.log($cells);
+            $cells.style.height = "110px";
+        }
+    }
+    startGame(hideSec, showSec);
 }
-
 
 // ===========================================
 function winCheck() {
     if (gradeSum >= 20) {
         alert("라운드 클리어!");
-        if (confirm("다음라운드 가시겠습니까")) {
-            gradeSum = 0;
-            round_test++;
-            printScore();
-            rowlevel++;
-            nextRound();  // 새 행, 시간 단축
-        } 
+        if (round == 3) {
+            alert('마지막 라운드까지 클리어하셨습니다. 축하드립니다!!!');
+            timer_stop();
+            hideImg();
+        } else {
+            if (confirm("다음라운드 가시겠습니까")) {
+                gradeSum = 0;
+                // round_test++;
+                printScore();
+                // rowlevel++;
+                nextRound();  // 새 행, 시간 단축
+            } else {
+                timer_stop();
+                time = 0;
+            }
+        }
     } else {
         alert("타임아웃!");
+        timer_stop();
+        hideImg();
     }
 }
 
@@ -168,12 +212,13 @@ function timeout() {
     // 화면에 타이머 출력
     time -= 1;
     document.querySelector(".timer").textContent = time;
-    if (time == 0 || gradeSum>=wingrade)
+    if (time == 0 || gradeSum >= wingrade)
         timer_stop();
-    
 }
 
 // 타이머 끝
 function timer_stop() {
+    time = 0;
     clearInterval(timerId);
+    hideImg();
 }
