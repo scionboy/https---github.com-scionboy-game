bff7f731a8fe41082802dd5471a8ded77003dbb9
dojuun2 <djp9816@naver.com>
2022-10-24 18:08:42 +0900

    게임 종료 처리 + 라운드 표시

--- 두더지잡기/app.js
+++ 두더지잡기/app.js
@@ -1,9 +1,17 @@
 let preNum = 1;     // 이전 랜덤숫자
-let hideSec = 2000; // 두더지 나왔다가 들어갈때까지의 시간
-let showSec = 3000; // 두더지 들어갔다가 나올때까지의 시간
+let hideSec = 1500; // 두더지 나왔다가 들어갈때까지의 시간
+let showSec = 2200; // 두더지 들어갔다가 나올때까지의 시간
 let round = 1;  // 라운드 누적 변수
 let num = 17;    // img의 class를 증가시킬 변수
 var rowlevel = 4;
+
+let startGameInterval;
+let $holes;
+let $td;
+
+const $round = document.getElementById('round');
+$round.textContent = round + '라운드';
+
 
 let wingrade = 10; // 다음라운드 점수
 
@@ -66,50 +74,69 @@ function hideImg() {
 
 // 게임 시작 함수
 function startGame(hideSecP, showSecP) {
+    $round.textContent = round + '라운드';
+
     if (confirm('게임 시작?')) {
+        console.log(time);
         gradeSum = 0;
         time = 10;
         inter_val();
-        let interval = setInterval(() => {
-            // 게임 종료 이벤트
-            document.getElementById('stop').addEventListener('click', function () {
-                if (confirm('게임을 종료하시겠습니까?')) {
-                    clearInterval(interval);    // 반복 중단
-                    timer_stop();
-                    document.querySelector(".timer").textContent = time;
-                    // gradeSum = 0;
-                    // hideImg();
-                    // printScore();
-                    // timer_stop();
-                }
-            })
+        startGameInterval = setInterval(() => {
+            console.log('호출');
             printImg(hideSecP);
             if (time == 0 || gradeSum >= wingrade) {
+                stopGame();
                 winCheck();
-                clearInterval(interval);    // 반복 중단
             }
+            // 게임 종료 이벤트
+            document.getElementById('stop').onclick =  function () {
+                if (confirm('게임을 종료하시겠습니까?')) {
+                    clearInterval(startGameInterval);    // 반복 중단
+                    alert('게임 종료!!');
+                    stopGame();
+                    initRound();
+                }
+            };
         }, showSecP);
     }
 }
-// // 게임 종료 함수
-// function stopGame() {
-//     if (confirm('게임을 종료하시겠습니까?')) {
-//         gradeSum = 0;
-//         hideImg();
-//         printScore();
-//         timer_stop();
-//     }
-// }
+
+function stopGame() {
+    timer_stop();
+    hideImg();
+    time = 0;
+    document.querySelector(".timer").textContent = time;
+}
+function initRound(){
+    gradeSum = 0;   // 점수 초기화
+    hideSec = 1500; // 두더지 나왔다가 들어갈때까지의 시간 
+    showSec = 2200; // 두더지 들어갔다가 나올때까지의 시간
+    if(round==2){
+        $map.deleteRow(-1);
+    }else if(round==3){
+        $map.deleteRow(-1);
+        $map.deleteRow(-1);
+    }
+    
+    for (let $hole of $holes) {
+        $hole.style.height = '50px';
+    }
+    for (let $cells of $td) {
+        $cells.style.height = "150px";
+    }
+    round=1;    // 라운드 초기화
+    $round.textContent = round + '라운드';
+
+}
 
 // 게임 시작 이벤트
 document.getElementById('start').addEventListener('click', function () {
     startGame(hideSec, showSec);    // 2000, 3000
 })
 
-
-
 // 다음 라운드 함수
 function nextRound() {
+    console.log(time);
     round++;
     const $map = document.querySelector('.map');// 테이블
 
@@ -123,8 +150,8 @@ function nextRound() {
         num++;
     }
 
-    const $holes = document.querySelectorAll('.hole');  // 구멍
-    const $td = document.querySelectorAll('td');   // td
+    $holes = document.querySelectorAll('.hole');  // 구멍
+    $td = document.querySelectorAll('td');   // td
     if (round == 2) {   // 2라운드 일때
         hideSec = 800;
         showSec = 1300;
@@ -132,7 +159,6 @@ function nextRound() {
             $hole.style.height = '45px';
         }
         for (let $cells of $td) {
-            console.log($cells);
             $cells.style.height = "130px";
         }
     } else if (round == 3) {    // 3라운드 일때
@@ -140,10 +166,8 @@ function nextRound() {
         showSec = 1000;
         for (let $hole of $holes) {
             $hole.style.height = '40px';
-            $hole.style.marginTop = '70px';
         }
         for (let $cells of $td) {
-            console.log($cells);
             $cells.style.height = "110px";
         }
     }
@@ -154,10 +178,12 @@ function nextRound() {
 function winCheck() {
     if (gradeSum >= 20) {
         alert("라운드 클리어!");
+        clearInterval(startGameInterval);    // 반복 중단
+
         if (round == 3) {
             alert('마지막 라운드까지 클리어하셨습니다. 축하드립니다!!!');
-            timer_stop();
-            hideImg();
+            stopGame();
+            initRound();
         } else {
             if (confirm("다음라운드 가시겠습니까")) {
                 gradeSum = 0;
@@ -166,19 +192,19 @@ function winCheck() {
                 // rowlevel++;
                 nextRound();  // 새 행, 시간 단축
             } else {
-                timer_stop();
-                time = 0;
+                stopGame();
+                initRound();
             }
         }
     } else {
-        alert("타임아웃!");
-        timer_stop();
-        hideImg();
+        alert("타임아웃! 클리어 실패 ㅠㅠㅠ");
+        clearInterval(startGameInterval);    // 반복 중단
+        stopGame();
+        initRound();
     }
 }
 
 function touch() {
-
     // 내가 누를때 시간
     var d = new Date();
 
@@ -204,7 +230,7 @@ function touch() {
 
 // 1초마다 timout()을 실행함
 function inter_val() {
-    timerId = setInterval("timeout()", 1000);
+    timerId = setInterval('timeout()', 1000);
 }
 
 // 1초씩 줄어드는 함수
@@ -218,7 +244,5 @@ function timeout() {
 
 // 타이머 끝
 function timer_stop() {
-    time = 0;
     clearInterval(timerId);
-    hideImg();
 }
